5107d9d933f85aabaf67cafda6c540dd8d9223f8
paul <pauljregnier@gmail.com>
2025-01-05 15:57:02 -0800

    Drive Setup (PhotonVision and PathPlanner TBA)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,16 +4,154 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int k_DriverControllerPort = 0;
+  }
+
+    public static final class DriveConstants {
+    // Driving Parameters - Note that these are not the maximum capable speeds of
+    // the robot, rather the allowed maximum speeds
+    public static final double k_MaxSpeedMetersPerSecond = 4.8;
+    public static final double k_MaxAngularSpeed = 2 * Math.PI; // radians per second
+
+    // Angular offsets of the modules relative to the chassis in radians
+    public static final double k_FLChassisAngularOffset = -Math.PI / 2;
+    public static final double k_FRChassisAngularOffset = 0;
+    public static final double k_BLChassisAngularOffset = Math.PI;
+    public static final double k_BRChassisAngularOffset = Math.PI / 2;
+
+    public static double k_FLOffset = 0 + k_FLChassisAngularOffset;
+    public static double k_FROffset = 0 + k_FRChassisAngularOffset;
+    public static double k_BLOffset = 0 + k_BLChassisAngularOffset;
+    public static double k_BROffset = 0 + k_BRChassisAngularOffset;
+
+    // SPARK MAX CAN IDs
+    public static final int k_FRTurningMotor = 4;
+    public static final int k_FLTurningMotor = 2;
+    public static final int k_BRTurningMotor = 6;
+    public static final int k_BLTurningMotor = 8;
+
+    public static final int k_FRDriveMotor = 3;
+    public static final int k_FLDriveMotor = 1;
+    public static final int k_BRDriveMotor = 5;
+    public static final int k_BLDriveMotor = 7;
+
+    public static final double k_rotateF = 0;
+    public static final double k_rotateP = .016;
+    public static final double k_rotateI = .05;// .01;
+    public static final double k_rotateD = .0008;
+    public static final double k_rotateIZone = 20; // 70
+    public static final double k_rotateDeadzone = 2;
+    public static final double k_maxRotInput = .8;
+
+    public static double k_maxSpeedMetersPerSecond = 5.74;
+    public static final double k_maxDriveAcceleration = 7.427;
+    public static final double k_maxAngularSpeed = Math.PI * 1.5; // radians per second
+    public static final double k_maxAngularAcceleration = Math.PI;
+
+    public static final double k_directionSlewRate = 3; // radians per second
+    public static final double k_magnitudeSlewRate = 3.25; // percent per second (1 = 100%)
+    public static final double k_rotationalSlewRate = 3; // percent per second (1 = 100%)
+
+    public static final SwerveModuleState[] k_defaultState = {
+        new SwerveModuleState(0, new Rotation2d(Math.PI / 4)),
+        new SwerveModuleState(0, new Rotation2d(3 * Math.PI / 4)),
+        new SwerveModuleState(0, new Rotation2d(3 * Math.PI / 4)),
+        new SwerveModuleState(0, new Rotation2d(Math.PI / 4))
+    };
+
+    public static final double k_lookAheadTimeSeconds = .2;
+
+    public static final boolean k_GyroReversed = false;
+  }
+
+  public static final class ModuleConstants {
+    // The MAXSwerve module can be configured with one of three pinion gears: 12T,
+    // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
+    // more teeth will result in a robot that drives faster).
+    public static final int k_DrivingMotorPinionTeeth = 14;
+
+    // Calculations required for driving motor conversion factors and feed forward
+    public static final double k_DrivingMotorFreeSpeedRps = 6784 / 60;
+    public static final double k_WheelDiameterMeters = 0.0762;
+    public static final double k_WheelCircumferenceMeters = k_WheelDiameterMeters * Math.PI;
+    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+    // teeth on the bevel pinion
+    public static final double k_DrivingMotorReduction = (45.0 * 22) / (k_DrivingMotorPinionTeeth * 15);
+    public static final double k_DriveWheelFreeSpeedRps = (k_DrivingMotorFreeSpeedRps * k_WheelCircumferenceMeters)
+        / k_DrivingMotorReduction;
+  }
+
+  public final class MotorConfigs {
+    public static final class SwerveModule {
+      public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
+      public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
+      public static SparkMaxConfig coastDriveConfig = new SparkMaxConfig();
+      public static SparkMaxConfig coastTurnConfig = new SparkMaxConfig();
+
+      static {
+        // Use module constants to calculate conversion factors and feed forward gain.
+        double drivingFactor = ModuleConstants.k_WheelDiameterMeters * Math.PI
+            / ModuleConstants.k_DrivingMotorReduction;
+        double turningFactor = 2 * Math.PI;
+        double drivingVelocityFeedForward = 1 / ModuleConstants.k_DriveWheelFreeSpeedRps;
+
+        drivingConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50);
+        drivingConfig.encoder
+            .positionConversionFactor(drivingFactor) // meters
+            .velocityConversionFactor(drivingFactor / 60.0); // meters per second
+        drivingConfig.closedLoop
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            // These are example gains you may need to them for your own robot!
+            .pid(0.04, 0, 0)
+            .velocityFF(drivingVelocityFeedForward)
+            .outputRange(-1, 1);
+
+        turningConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(20);
+        turningConfig.absoluteEncoder
+            // Invert the turning encoder, since the output shaft rotates in the opposite
+            // direction of the steering motor in the MAXSwerve Module.
+            .inverted(true)
+            .positionConversionFactor(turningFactor) // radians
+            .velocityConversionFactor(turningFactor / 60.0); // radians per second
+        turningConfig.closedLoop
+            .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+            // These are example gains you may need to them for your own robot!
+            .pid(1, 0, 0)
+            .outputRange(-1, 1)
+            // Enable PID wrap around for the turning motor. This will allow the PID
+            // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
+            // to 10 degrees will go through 0 rather than the other direction which is a
+            // longer route.
+            .positionWrappingEnabled(true)
+            .positionWrappingInputRange(0, turningFactor);
+        coastDriveConfig = drivingConfig;
+        coastTurnConfig = turningConfig;
+        
+        coastDriveConfig.idleMode(IdleMode.kCoast);
+        coastTurnConfig.idleMode(IdleMode.kCoast);
+      }
+    }
   }
 }
