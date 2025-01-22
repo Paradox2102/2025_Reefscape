c5769cafe1eb8b5b3379bbf0992094d22e6aa72d
Briselda <122310852+BrisMR@users.noreply.github.com>
2025-01-21 17:38:47 -0800

    first step into tests

---
+++ src/main/java/frc/robot/commands/Tests/TestCoralCommand.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Tests;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralOuttakeSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class TestCoralCommand extends Command {
+  private CoralOuttakeSubsystem m_subsystem;
+  private boolean m_forward;
+
+  /** Creates a new TestCoralCommand. */
+  public TestCoralCommand(CoralOuttakeSubsystem subsystem, boolean forward) {
+    m_subsystem = subsystem;
+    m_forward = forward;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (m_forward) {
+      m_subsystem.setPower(0.1);
+    } else {
+      m_subsystem.setPower(-0.1);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
