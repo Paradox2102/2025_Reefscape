80495a59947ee7907c2df85281f42064bd6368ff
paul <pauljregnier@gmail.com>
2025-02-12 15:59:54 -0800

    pathplanner setup

--- src/main/java/frc/robot/subsystems/CoralOuttakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralOuttakeSubsystem.java
@@ -15,11 +15,9 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.filter.Debouncer.DebounceType;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
