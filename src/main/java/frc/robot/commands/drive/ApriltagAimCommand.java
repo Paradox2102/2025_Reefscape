81aea9e2e15c864290e44bf37fb9dba1ec6bfc1a
paul <pauljregnier@gmail.com>
2025-03-09 16:45:28 -0700

    im dum

--- src/main/java/frc/robot/commands/drive/ApriltagAimCommand.java
+++ src/main/java/frc/robot/commands/drive/ApriltagAimCommand.java
@@ -71,6 +71,6 @@ public class ApriltagAimCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_horizDist < 0.05;
+    return Math.abs(m_horizDist) < 0.05;
   }
 }
